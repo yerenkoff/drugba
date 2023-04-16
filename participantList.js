99f52fb791b34cfcc15f52a92826bfc2b86f1eeb
Yerenkoff <yerenkoff@gmail.com>
2023-04-16 09:36:38 +0300

    design

--- participantList.js
+++ participantList.js
@@ -2,28 +2,20 @@ let startX, movingX, deltaX;
 let endX = 0
 let infos = document.getElementsByClassName("participantInfo")
 for (let participantInfo of infos) {
-    console.log(participantInfo);
     participantInfo.ontouchstart = (event) => {
         touchStart(event, participantInfo)
-        console.log(12);
-    } 
+    }
     participantInfo.ontouchmove = (event) => {
         touchMove(event, participantInfo)
-    } 
+    }
     participantInfo.ontouchend = () => {
         touchEnd(participantInfo)
-    } 
+    }
 }
 
 function touchStart(e, participantInfo) {
     participantInfo.style.transition = "0s"
-    console.log(e.touches[0].clientX);
-    // if (endX != 0) {
-    // startX = endX
-    // } else {
     startX = e.touches[0].clientX
-
-    // }
 }
 
 function touchMove(e, participantInfo) {
@@ -31,7 +23,6 @@ function touchMove(e, participantInfo) {
     console.log(e.touches[0].clientX);
     movingX = e.touches[0].clientX
     deltaX = movingX - startX
-    // p.innerHTML = deltaX
     if (deltaX <= -48) {
         deltaX = -48
     }
@@ -39,14 +30,12 @@ function touchMove(e, participantInfo) {
         console.log();
         participantInfo.style.transform = "translateX(" + deltaX + "px)"
     }
-    
+
 }
 
 function touchEnd(participantInfo) {
     console.log(participantInfo);
     if (deltaX <= -48) {
-        // endX = movingX
-        // console.log(endX);
         setTimeout(() => {
             participantInfo.style.transition = "0.3s"
             participantInfo.style.transform = "translateX(-" + 0 + "px)"
@@ -57,5 +46,3 @@ function touchEnd(participantInfo) {
     }
 
 }
-
-// touchMove()
